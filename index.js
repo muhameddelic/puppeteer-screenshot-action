ce76c071176400b044c12a858b5ca4514cfa6b2d
Muhamed Delic <muhamed.delicc@live.com>
2019-10-01 19:05:00 +1000

    Add width and height to inputs

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const io = require("@actions/io");
 (async () => {
   await io.mkdirP(`${process.env.GITHUB_WORKSPACE}/screenshots/`);
 
-  let url = core.getInput("url");
+  const url = core.getInput("url");
+  const width = core.getInput("width");
+  const height = core.getInput("height");
 
   const timestamp = new Date().getTime();
 
   const browser = await puppeteer.launch({
     executablePath: "/usr/bin/google-chrome",
-    defaultViewport: { width: 1920, height: 1080 }
+    defaultViewport: { width, height }
   });
   const page = await browser.newPage();
   await page.goto(url, {
